49fe34b15ec8710f69471152f7fac2fd03a48d99
Nobody2-cyp <Lucius_seow_wen_jun@students.edu.sg>
2026-07-06 10:58:05 +0000

    Jul 6, 2026, 10:58 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -46,5 +46,16 @@ fill(0);
 Text("Ball:  ("+int(ball.x)+", "+ int(ball.y) + ")", 10, 20);
 
 text("Mouse: (" + mouseX + ", " + mouseY + ")", 10, 40);
-if (ball.x < 0 + ball.diameter / 2 || ball.x >width - ball.diameter / 2)
+if (ball.x < 0 + ball.diameter / 2 || ball.x > width - ball.diameter / 2){
+    ball.vel.x *= -1;
+}
+
+if (ball.y < 0 + ball.diameter / 2 || ball.y > height - ball.diameter / 2) {
+  ball.vel.y *=-1;
+}
+
+
+box.x = mouseX;
+box.y = mouseY;
+
 }
