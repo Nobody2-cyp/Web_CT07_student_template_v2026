9b4209e78fd3e05fa4395e4d725ce741e1383ccb
Nobody2-cyp <Lucius_seow_wen_jun@students.edu.sg>
2026-07-06 10:04:27 +0000

    Jul 6, 2026, 10:04 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -3,6 +3,8 @@ function setup() {
   new Canvas(800, 400);
   background(250); //background color
   FileList("skyblue");
+  stroke("pink")
+  
 
   // Basic shape testing
   // write your codes here
