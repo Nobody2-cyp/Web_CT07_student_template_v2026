ed8412369e115ba479ce19f00bcd9a038aea600f
Nobody2-cyp <Lucius_seow_wen_jun@students.edu.sg>
2026-07-13 09:51:21 +0000

    Jul 13, 2026, 9:51 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -15,5 +15,6 @@ function setup() {
 
   // --- Exercise: Array operations (groceries) ---
   // write your codes here
-}  new Canvas(800, 400);
-  background(250);
+}
+new Canvas(800, 400);
+background(250);
