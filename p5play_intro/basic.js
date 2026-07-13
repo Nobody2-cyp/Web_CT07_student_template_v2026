f2e9fe327407fe7418306ee50d30baa3a94927fd
Nobody2-cyp <Lucius_seow_wen_jun@students.edu.sg>
2026-07-13 10:42:23 +0000

    Jul 13, 2026, 10:42 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -39,8 +39,7 @@ console.log("upper primary");
   // --- Exercise}: Display odd numbers backward using while loop ---
   // write your codes here
 let count = 19
-while (count <0
-  
+while (count >1
 ){
   console.log("Counter:", count);
   count++;
