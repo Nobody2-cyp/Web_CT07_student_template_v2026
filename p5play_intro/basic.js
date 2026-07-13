311c688fe9f3ae2dcb94d14b715b0f8ab185a348
Nobody2-cyp <Lucius_seow_wen_jun@students.edu.sg>
2026-07-13 10:38:10 +0000

    Jul 13, 2026, 10:38 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -39,7 +39,9 @@ console.log("upper primary");
   // --- Exercise}: Display odd numbers backward using while loop ---
   // write your codes here
 let count = 0
-while (count < 5)
+while (count < 5){
+  console.log("")
+}
   // --- Exercise: Array operations (groceries) ---
   // write your codes here
 }
