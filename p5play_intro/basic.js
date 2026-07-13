b539cedf657cf8c5408cba9368effb217a8a9b8f
Nobody2-cyp <Lucius_seow_wen_jun@students.edu.sg>
2026-07-13 10:37:04 +0000

    Jul 13, 2026, 10:37 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -26,7 +26,8 @@ text(totalsum,67,300)
 
   // --- Exercise: Age category classification ---
   // write your codes here
-let age = 60;
+let age = 60
+
 
 if(age<= 9){
   console.log("lower primary");
