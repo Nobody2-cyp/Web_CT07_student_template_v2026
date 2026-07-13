210df1c4fdb29e34f55afba9a40dc5c1165fad83
Nobody2-cyp <Lucius_seow_wen_jun@students.edu.sg>
2026-07-13 10:54:26 +0000

    Jul 13, 2026, 10:54 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -48,7 +48,7 @@ groceries.push("orange")
 groceries.push("butter")
 groceries.shift()
 groceries.splice(1,1,"kaya")
-
+groceries.len
   // --- Exercise: Array operations (groceries) ---
   // write your codes here
 }
