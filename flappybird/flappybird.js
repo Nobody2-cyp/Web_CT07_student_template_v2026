2d32f6285372cdf260c275d48113b530f67b5851
Nobody2-cyp <Lucius_seow_wen_jun@students.edu.sg>
2026-07-20 11:04:47 +0000

    Jul 20, 2026, 11:04 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -1,5 +1,5 @@
 let bird, floor;
-letflapMidImg, bg, BaseAudioContext;
+letflapMidImg, bg;
 
 function preload() {
     //bird image, background and the floor
@@ -9,8 +9,18 @@ function preload() {
 function setup() {
     new Canvas(400, 600);
 
+    floor = new Sprite();
+    floor.x = 200;
+    floor.y = height - 20;
+    floor.width = 400;
+    floor.height = 125;
+    floor.collider = "static";
+    floor.img = base; 
 
-    bird.collider = "d"
+    bird.collider = "dynamic";
+    bird.mass = 2;
+    bird.drag = 0.02;
+   world.gravity.y = 10;
     bird = new bird = new Sprite();
     bird.x = width / 2;
     bird.y = 200
@@ -20,5 +30,15 @@ function setup() {
     }
     function draw(){
         Image(bg, 0, 0, width, height);
+        
+        if (kb.presses('space')) {
+            bird.vel.y = -15;
+            bird.sleeping = false
+            fill("blue");
+            textSize(14);
+            text('vel.y: ' + bird.vel.y.toFixed(2), 10, 20);
+            text( 'isMoving: ' + bird.isMoving, 10, 40);
+            text('sleeping: ' + bird.sleeping , 10, 60);
+        }
     }
 }
