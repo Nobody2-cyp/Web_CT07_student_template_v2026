e1696f8d9362faaec6dd00b096eefad055367e08
Nobody2-cyp <Lucius_seow_wen_jun@students.edu.sg>
2026-07-20 10:36:07 +0000

    Jul 20, 2026, 10:36 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -2,7 +2,7 @@ let bird, floor;
 letflapMidImg, bg, BaseAudioContext;
 
 function preload() {
-    //bird image, background
+    //bird image, background and the floor
     flapMidImg = loadImage('assets/yellowbird-midflap.png');
     bg = loadImage('assets/background-day.png');
     base = loadImage('assets/base.png');
