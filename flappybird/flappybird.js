67aeb5eb81b669422720808efa9ad0b7fec713b1
Nobody2-cyp <Lucius_seow_wen_jun@students.edu.sg>
2026-07-20 10:38:10 +0000

    Jul 20, 2026, 10:38 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -7,7 +7,7 @@ function preload() {
     bg = loadImage('assets/background-day.png');
     base = loadImage('assets/base.png');
 function setup() {
-    new Canvas(400, 600)
-    
+    new Canvas(400, 600);
+
     }
 }
