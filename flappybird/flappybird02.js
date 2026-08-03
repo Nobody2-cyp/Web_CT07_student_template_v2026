409acaf4b3ac0306db5655a3a7bb7af2b8b3f317
Nobody2-cyp <Lucius_seow_wen_jun@students.edu.sg>
2026-08-03 10:38:59 +0000

    Aug 3, 2026, 10:38 AM

--- flappybird/flappybird02.js
+++ flappybird/flappybird02.js
@@ -66,7 +66,7 @@ function draw(){
         startGame = true;
         startScreenLabel.visible = false;
     }
-    if ()
+    if (startGame)
     if (bird.collides(pipeGroup) || bird.collides(floor)){
         gameoverLabel = new Sprite(width/2, height/2, 192, 42);
         gameoverLabel.img = gameoverImg;
