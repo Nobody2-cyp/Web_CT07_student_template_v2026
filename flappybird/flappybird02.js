9c03d9fd2274941f470ca4d8744ef0a89b5dd84f
Nobody2-cyp <Lucius_seow_wen_jun@students.edu.sg>
2026-08-03 10:28:34 +0000

    Aug 3, 2026, 10:28 AM

--- flappybird/flappybird02.js
+++ flappybird/flappybird02.js
@@ -10,7 +10,7 @@ let startScreenLabel;
 let startScreemImg;
 function preload() {
     //bird image, background and the floor
-    
+    startScreenImg
     gameoverImg = loadImage('assets/gameover.png')
     flapMidImg = loadImage('assets/yellowbird-midflap.png');
     flapUpImg = loadImage('assets/yellowbird-upflap.png');
