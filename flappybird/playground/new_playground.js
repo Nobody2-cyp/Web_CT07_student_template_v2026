b232173d778d41b3ece3210ebe303f0886412d2b
Nobody2-cyp <Lucius_seow_wen_jun@students.edu.sg>
2026-08-20 10:49:36 +0000

    Aug 20, 2026, 10:49 AM

--- flappybird/playground/new_playground.js
+++ flappybird/playground/new_playground.js
@@ -1,6 +1,7 @@
 let digitImgs =[];
 let digitLabel;
-let score = 7;
+let score = 1;
+let ScoreGroup;
 function preload() {
     digitImg = loadImage("assets/7.png");
     let prefix = "assets/";
@@ -22,6 +23,9 @@ function setup() {
     digitLabel.h = 36;
     digitLabel.collider = "none";
     digitLabel.img = digitImgs[score];
+
+    scoreGroup = new Group();
+    scoreGroup.collider = "none";
 }
 function draw() {
     background("skyblue");
@@ -29,25 +33,29 @@ function draw() {
     textSize(14);
     text("score: " + score, 50, 50);
     text("frameCount: " + frameCount, 50, 70);
-
+}
 // add keyboard events
 // up arrow to increase score
 // down arrow to decrease score
 if (kb.pressed("up")) {
     score++;
 }
-if (kb.pressed("down")) {
+else if (kb.pressed("down")) {
     score--;
-
 }
-score = constrain(score, 0, 9);
+else if (kb.presses("2")) {
+
 
 }
 function drawScore() {
     let scoreStr = str(score);
     let digitArray = scoreStr.split("");
-    print(digitArray.length);
-    
+    // print(digitArray.length);
+    // print(digitArray[0]);
+    // print(digitArray[1]);
+
+    // if (score > 10) {
+    //     noLoop();
 }
 function moveGroup() {
 }
