3a0f40aad493ff47e54b8d4d2575b907a06e95ac
Nobody2-cyp <Lucius_seow_wen_jun@students.edu.sg>
2026-08-24 10:04:59 +0000

    Aug 24, 2026, 10:04 AM

--- flappybird/playground/flappy bird 04.js
+++ flappybird/playground/flappy bird 04.js
@@ -105,6 +105,7 @@ function draw() {
     if (kb.presses('space') || mouse.presses()) {
       bird.vel.y = -5; // which direction do you think this is?
       bird.sleeping = false; // wake up if sleeping
+      
     }
     
     // Activity: Change image according to flying action/ falling
