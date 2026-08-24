2d37c0b3fa5e25a98cb3a7fbd8fed6d65586d621
Nobody2-cyp <Lucius_seow_wen_jun@students.edu.sg>
2026-08-24 09:57:50 +0000

    Aug 24, 2026, 9:57 AM

--- flappybird/playground/flappy bird 04.js
+++ flappybird/playground/flappy bird 04.js
@@ -15,7 +15,7 @@ let startScreenImg; // declare variable for image
 let score = 0;
 let numberImages = []; // store number/score images
 let scoreDigits; // group for storing the different numbers
-let Fl
+let flapSoun
 
 function preload() {
     // bird image, background and the floor
