a735ab4539d63e5a8de968661468f7b963902103
Nobody2-cyp <Lucius_seow_wen_jun@students.edu.sg>
2026-08-24 09:18:27 +0000

    Aug 24, 2026, 9:18 AM

--- flappybird/playground/new_playground.js
+++ flappybird/playground/new_playground.js
@@ -1,78 +1,87 @@
-let digitImgs =[];
+// 3 functions needed for p5play
+
+let digitImgs = []; // change to array
 let digitLabel;
-let score = 1;
-let ScoreGroup;
+let score = 1; // to keep track
+let scoreGroup; // group to manage the digits for score above 100
+
 function preload() {
-    digitImg = loadImage("assets/7.png");
+    //digitImg = loadImage("assets/7.png");
+
     let prefix = "assets/";
     let suffix = ".png";
     let filename = "";
 
     for (let count=0; count<10; count++) {
-        filename = prefix + count + suffix;
-        //print(filename);
-        digitImgs[count] = loadImage(filename);
+      filename = prefix + count + suffix;
+      //print(filename);
+      digitImgs[count] = loadImage(filename);
     }
 }
+
+// called once
 function setup() {
     new Canvas(400, 600);
 
     digitLabel = new Sprite();
     digitLabel.y = 50;
-    digitLabel.w = 25;
-    digitLabel.h = 36;
+    digitLabel.width = 25; // sprite.w
+    digitLabel.height = 36; // sprite.h
     digitLabel.collider = "none";
-    digitLabel.img = digitImgs[score];
+    //digitLabel.img = digitImgs[score]; // load the correct image using the array
 
     scoreGroup = new Group();
     scoreGroup.collider = "none";
 }
+
+// forever loop
 function draw() {
-    background("skyblue");
-    digitLabel.img = digitImgs[score];
+    background("skyblue"); // erase
+    
+    // add debugging to show score
     textSize(14);
-    text("score: " + score, 50, 50);
-    text("frameCount: " + frameCount, 50, 70);
+    text("score: " + score,           50,50);
+    text("frameCount: " + frameCount, 50,70);
 
-// add keyboard events
-// up arrow to increase score
-// down arrow to decrease score
-if (kb.pressed("up")) {
-    score++;
-}
-else if (kb.pressed("down")) {
-    score--;
-}
-else if (kb.presses("2")) {
-    score = round(random(10,99));
+    //digitLabel.img = digitImgs[score]; // load the correct image using the array
 
+    // add keyboard event
+    // up arrow to increase score
+    // down arrow to decrease score
+    if (kb.presses("up")) {
+        score = score +1;
+    }
+    else if (kb.presses("down")) {
+        score = score -1;
+    }
+    else if (kb.presses("2")) {
+        score = round(random(10,99));
+    }
 
-
-}
+    score = constrain(score,0,99); // keep the variable within the digits
+    drawScore();
 }
 
 function drawScore() {
-    let scoreStr = str(score);
-    let digitArray = scoreStr.split("");
-    // print(digitArray.length);
-    // print(digitArray[0]);
-    // print(digitArray[1]);
+    scoreGroup.removeAll(); // clear the members
 
-    // if (score > 10) {
-    //     noLoop();
-    let middle = width/2;
+    let scoreString = str(score);
+    let digitArray = scoreString.split(""); // split at every character
+
+    let middle = width/2; // make sure on the center of the canvas
     let offset = 0;
-    for(let one of digitArray) {
+    for (let one of digitArray) {
         let s = new Sprite();
         s.img = digitImgs[one];
         s.x = middle + offset;
+        //s.y = 50;
         s.w = 25;
         s.h = 36;
 
-        offset += 25;
+        offset = offset + 25;
         scoreGroup.add(s);
-        
     }
 }
+
 function moveGroup() {
 }
