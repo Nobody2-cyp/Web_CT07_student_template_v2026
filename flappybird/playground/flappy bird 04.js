73fa1bbce6da504dc5becf39ab48c8a71ba458f6
Nobody2-cyp <Lucius_seow_wen_jun@students.edu.sg>
2026-08-24 09:59:32 +0000

    Aug 24, 2026, 9:59 AM

--- flappybird/playground/flappy bird 04.js
+++ flappybird/playground/flappy bird 04.js
@@ -35,7 +35,7 @@ function preload() {
 
     for (let i = 0; i < 10; i++){
       numberImages[i] = loadImage('assets/' + i +'.png');
-      flapSound = createAudio('assets/sf')
+      flapSound = createAudio('assets/sfx_wing.mp3')
     }
 }
 
