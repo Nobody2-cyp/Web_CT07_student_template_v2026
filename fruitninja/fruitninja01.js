be5d34f492da62ae5f9de736129898479d4e55e4
Nobody2-cyp <Lucius_seow_wen_jun@students.edu.sg>
2026-09-12 07:29:34 +0000

    Sep 12, 2026, 7:29 AM

--- fruitninja/fruitninja01.js
+++ fruitninja/fruitninja01.js
@@ -73,8 +73,8 @@ function splitFruit(x, y, fruitData){
     let left = new fruitHalves.Sprite(x, y, 20);
     left.image = fruitData.half1;
     left.vel.x = -3;
-    half1.vel.y = random(-5, -2);
-    half1.rotationSpeed = -5;
+    left.vel.y = random(-5, -2);
+    left.rotationSpeed = -5;
     left.life = 30;
     
     let right = new fruitHalves.Sprite(x+ 10, y, 40, 40);
