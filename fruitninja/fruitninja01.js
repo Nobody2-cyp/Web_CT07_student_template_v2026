6470a66dd43300d946040cda8a97ee677053b481
Nobody2-cyp <Lucius_seow_wen_jun@students.edu.sg>
2026-09-12 07:10:07 +0000

    Sep 12, 2026, 7:10 AM

--- fruitninja/fruitninja01.js
+++ fruitninja/fruitninja01.js
@@ -24,13 +24,14 @@ function draw(){
     image(dojoBG, 0, 0, width, height);
     if (frameCount %120 === 0){
         spawnFruit();
-    }
-     if (mouse.pressing){
+         if (mouse.pressing){
             trail = new Sprite(mouse.x, mouse.y, 7);
             trail.collider = 'none';
             trail.color = 'blue';
             trail.life = 10;
     }
+    }
+    
 }
 
 function spawnFruit(){
