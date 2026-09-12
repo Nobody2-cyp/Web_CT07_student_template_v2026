3eb7eea8bc008897087fd34daeae13a64aa5a79e
Nobody2-cyp <Lucius_seow_wen_jun@students.edu.sg>
2026-09-12 07:14:28 +0000

    Sep 12, 2026, 7:14 AM

--- fruitninja/fruitninja01.js
+++ fruitninja/fruitninja01.js
@@ -7,12 +7,12 @@ function preload(){
      let peach = {
         whole: loadImage('assets/peachwhole.png'),
         half1: loadImage('assets/peachhalf.png'),
-        half2: loadImage('assets/peachhalf.png')
+        half2: loadImage('assets/peachhalf.png'),
     };
     let watermelon = {
         whole: loadImage('assets/watermelonwhole.png'),
         half1: loadImage('assets/watermelonhalf.png'),
-        half2: loadImage('assets/watermelonhalf.png')
+        half2: loadImage('assets/watermelonhalf.png'),
     }
     fruitTypes = [peach, watermelon];
 }
